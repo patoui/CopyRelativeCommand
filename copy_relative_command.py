327a1a472a013edf3e6364bd06a9cee7cacb4a32
Patrique Ouimet <patrique.ouimet@gmail.com>
2023-05-16 08:09:21 -0400

    chore: improve language detection, remove leading space

--- copy_relative_command.py
+++ copy_relative_command.py
@@ -7,7 +7,7 @@ class CopyRelativeCommandCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         filename = self.view.file_name()
         if len(filename) > 0:
-            shortest_relative_path = min(
+            relative_path = min(
                 (
                     relpath(filename, folder)
                     for folder in sublime.active_window().folders()
@@ -22,16 +22,18 @@ class CopyRelativeCommandCommand(sublime_plugin.TextCommand):
                     command_parts = command.get('command')
                     glob_pattern = command.get('glob', '*')
 
-                    if fnmatch.fnmatch(shortest_relative_path, glob_pattern):
+                    if fnmatch.fnmatch(relative_path, glob_pattern):
                         command = ''
                         for part in command_parts:
                             if '{' in part:
                                 part = part.format(
-                                    relative_path=shortest_relative_path,
-                                    function=self.get_current_function())
-                            command += ' ' + part
+                                    relative_path=relative_path,
+                                    current_function=self.get_current_function(),
+                                    current_class=self.get_current_class(),
+                                    current_function_or_class=self.get_current_function_or_class())
+                            command += part + ' '
 
-                        sublime.set_clipboard(command)
+                        sublime.set_clipboard(command.rstrip())
                         sublime.status_message("Copied relative command!")
                         return
 
@@ -43,32 +45,45 @@ class CopyRelativeCommandCommand(sublime_plugin.TextCommand):
     def language_settings(self, name):
         languages = self.get_setting('languages')
 
-        if languages:
-            if name in languages:
-                return languages[name]
+        if not languages or name not in languages:
+            return None
 
-        return None
+        return languages[name]
 
     def get_language(self):
-        symbol_regions = self.view.symbol_regions()
+        syntax = self.view.syntax()
 
-        if not symbol_regions:
+        if not syntax:
             return ''
 
-        # TODO: find better solution for determine language/source/syntax
-        return symbol_regions[0].syntax.replace(' Source', '').lower()
+        return syntax.name.lower()
 
     def get_setting(self, setting_name, default_value=None):
         settings = sublime.load_settings("CopyRelativeCommand.sublime-settings")
         return settings.get(setting_name, default_value)
 
-    def get_current_function(self):
-        selector = self.view.sel()[0]
-        function_regions = self.view.find_by_selector('entity.name.function')
-        current_function = None
+    def get_current_function_or_class(self):
+        current_function = self.get_current_function()
 
-        for region in reversed(function_regions):
+        if len(current_function) > 0:
+            return current_function
+
+        return self.get_current_class()
+
+    def get_current_class(self):
+        return self.get_current_scope('entity.name.class')
+
+    def get_current_function(self):
+        return self.get_current_scope('entity.name.function')
+
+    def get_current_scope(self, scope):
+        selector = self.view.sel()[0]
+        scope_regions = self.view.find_by_selector(scope)
+        current_scope = ''
+
+        for region in reversed(scope_regions):
             if region.a < selector.a:
-                current_function = self.view.substr(region)
+                current_scope = self.view.substr(region)
                 break
-        return current_function
+
+        return current_scope
